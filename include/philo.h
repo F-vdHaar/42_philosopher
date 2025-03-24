8494ea6d1bc105abe71c6c86caa5a9136325fc7c
fvon-der <fvon-der@student.42heilbronn.de>
2025-03-24 10:53:32 +0100

    Basic structure
    Structs : table, philosophers

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 10:18:59 by fvon-de           #+#    #+#             */
-/*   Updated: 2025/03/24 10:28:09 by fvon-de          ###   ########.fr       */
+/*   Updated: 2025/03/24 10:46:11 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,53 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <stdio.h>
+
+#include <pthread.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <stdio.h>
+
+typedef struct s_table	t_table;
+
+typedef struct s_philosopher
+{
+	int				id;
+	int				meals_eaten;
+	long			last_meal_time;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_table			*table;
+}	t_philosopher;
+
+struct s_table
+{
+	int				num_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				meals_required;
+	int				stop_simulation;
+	long			start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	meal_lock;
+	t_philosopher	*philos;
+};
 
 extern int	g_debug_mode;
+
+// Table
+void	free_table(t_table *table);
+int	init_table(t_table *table, int argc, char **argv);
+
+
 
 // gen_utils
 void		log_error(const char *message);
 void		enable_debug_mode(void);
 void		log_output(const char *message);
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int         ft_atoi(const char *str);
+int         ft_strncmp(const char *s1, const char *s2, size_t n);
 #endif
--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 10:31:53 by fvon-de           #+#    #+#             */
-/*   Updated: 2025/03/24 10:32:42 by fvon-de          ###   ########.fr       */
+/*   Updated: 2025/03/24 10:51:58 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ static int	handle_special_args(char *arg)
 {
 	if (ft_strncmp(arg, "--help", 6) == 0)
 	{
-		write(1, "Usage: ./philo num_philos time_to_die time_to_eat time_to_sleep [num_meals]\n", 74);
-		write(1, "Simulates the dining philosophers problem.\n", 42);
+		write(1, "Usage: ./philo num_philos time_to_die time_to_eat time_to_sleep [num_meals]\n", 77);
 		return (EXIT_SUCCESS);
 	}
 	if (ft_strncmp(arg, "--debug", 7) == 0)
@@ -35,7 +34,7 @@ static int	handle_special_args(char *arg)
 
 int	main(int argc, char *argv[])
 {
-	//t_table	table;
+	t_table	table;
 
 	if (argc == 2)
 		return (handle_special_args(argv[1]));
@@ -44,13 +43,13 @@ int	main(int argc, char *argv[])
 		write(2, "Error: Invalid number of arguments. Type --help\n", 48);
 		return (EXIT_FAILURE);
 	}
-	// if (init_table(&table, argc, argv) == -1)
-	// {
-	// 	write(2, "Error: Initialization failed\n", 29);
-	// 	return (EXIT_FAILURE);
-	// }
+	if (init_table(&table, argc, argv) == -1)
+	{
+		write(2, "Error: Initialization failed\n", 29);
+		return (EXIT_FAILURE);
+	}
 	//start_simulation(&table);
-	//free_table(&table);
+	free_table(&table);
 	return (EXIT_SUCCESS);
 }
 
